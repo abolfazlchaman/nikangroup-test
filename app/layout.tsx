4e86fc9c05d813fbd269e5bce159bbd6f72609fc
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-05-23 17:41:41 +0330

    fixed a viewport error deprecation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Providers } from './providers';
 import './globals.css';
 import { Roboto } from 'next/font/google';
 import Navbar from '@/app/components/Navbar';
 import { Footer } from './components/Footer';
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor: '#000000',
+};
 
 export const metadata: Metadata = {
   title: 'Nikan Group Test | Abolfazl Chaman',
@@ -14,13 +21,32 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico' },
+      { url: '/favicon.ico', sizes: 'any' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
     apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
   },
   manifest: '/site.webmanifest',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Nikan Group Test',
+  },
 };
 
 const roboto = Roboto({
