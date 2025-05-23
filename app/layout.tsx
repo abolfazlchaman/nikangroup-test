b0428aa81f6bbdde28dc035446671664282f4c81
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-05-23 12:41:13 +0330

    implemented dark and light mode basis with tailwind, removed no longer needed theme.tsfrom MUI and its imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import type { Metadata } from 'next';
+import { Providers } from './providers';
+import { Header } from './components/header';
 import './globals.css';
 import { Roboto } from 'next/font/google';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
 
 export const metadata: Metadata = {
   title: 'Nikan Group Test | Abolfazl Chaman',
@@ -21,19 +20,17 @@ const roboto = Roboto({
   variable: '--font-roboto',
 });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html
       lang='en'
+      suppressHydrationWarning
       className={roboto.variable}>
-      <body>
-        <ThemeProvider theme={theme}>
-          <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
-        </ThemeProvider>
+      <body className='min-h-screen bg-background font-sans antialiased'>
+        <Providers>
+          <Header />
+          <main className='container mx-auto px-4 py-8'>{children}</main>
+        </Providers>
       </body>
     </html>
   );
