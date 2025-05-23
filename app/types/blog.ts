55b56b0a6b36ca8fd64cb3db92083bb93b4d6781
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-05-23 13:25:58 +0330

    added types for typescript for the articles (blog) section

---
+++ app/types/blog.ts
@@ -0,0 +1,13 @@
+export interface BlogPost {
+  id: number;
+  title: string;
+  body: string;
+  imageUrl: string;
+}
+
+export interface PaginatedResponse {
+  posts: BlogPost[];
+  total: number;
+  page: number;
+  limit: number;
+} 
