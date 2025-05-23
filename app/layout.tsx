7259fc624b62626703a6b063f96a41b43c20e67c
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-05-23 12:46:52 +0330

    added favicons metadata generation to layout for SSR

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,15 @@ export const metadata: Metadata = {
     index: false,
     follow: false,
   },
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+  },
+  manifest: '/site.webmanifest',
 };
 
 const roboto = Roboto({
